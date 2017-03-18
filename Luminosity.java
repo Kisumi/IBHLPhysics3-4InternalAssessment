fbbd6b94dc36a4b94b73eca65315755e24c3c1f3
Elaine Yang <elaineyangx.gmail.com>
2017-03-18 02:34:57 -0700

    fixed luminosities again

--- Luminosity.java
+++ Luminosity.java
@@ -3,7 +3,7 @@ import java.io.File;
 import java.io.PrintWriter;
 
 class Luminosity {
-	static int I = 868151, II = 125819, III = 18235, IV = 2642, V = 383, VI = 55, VII = 8; //pre-calculated values that correspond to the luminosity class
+	static int III = 43279, IV = 2873, V = 383, VII = 161; //pre-calculated values that correspond to the luminosity class
 
 	public static void main(String[] args) {
 		int valueOfLum = 0;
@@ -16,18 +16,18 @@ class Luminosity {
 			for(int i = 0; i < 106; i++) {
 				lums = luminosities.nextLine();
 				switch(lums) {
-					case "I": valueOfLum = I;
-						break;
-					case "II": valueOfLum = II;
-						break;
+					//case "I": valueOfLum = I;
+					//	break;
+					//case "II": valueOfLum = II;
+					//	break;
 					case "III": valueOfLum = III;
 						break;
 					case "IV": valueOfLum = IV;
 						break;
 					case "V": valueOfLum = V;
 						break;
-					case "VI": valueOfLum = VI;
-						break;
+					//case "VI": valueOfLum = VI;
+					//	break;
 					case "VII": valueOfLum = VII;
 						break;
 				}
